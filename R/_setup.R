0199d2963b9134d4424772cbccf56bacd32a7b47
Daniel Vartanian <danielvartan@proton.me>
2025-08-22 10:28:37 -0300

    Remove and reduce final sections

--- R/_setup.R
+++ R/_setup.R
@@ -1,15 +1,16 @@
 # Load packages -----
 
-# library(brandr)
+library(brandr)
 library(downlit)
 library(ggplot2)
-# library(here)
+library(here)
 library(httpgd) # github.com/nx10/httpgd
-# library(knitr)
+library(knitr)
 library(magrittr)
+library(quartor) # github.com/danielvartan/quartor
 library(ragg)
 library(rlang)
-# library(rutils) # github.com/danielvartan/rutils
+library(rutils) # github.com/danielvartan/rutils
 library(xml2)
 
 # Set general options -----
@@ -32,12 +33,12 @@ set.seed(2025)
 
 # Set knitr -----
 
-knitr::clean_cache() |> rutils:::shush()
+clean_cache() |> shush()
 
-knitr::opts_chunk$set(
+opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  root.dir = here::here(),
+  root.dir = here(),
   dev = "ragg_png",
   dev.args = list(bg = "transparent"),
   fig.showtext = TRUE
@@ -54,9 +55,9 @@ knitr::opts_chunk$set(
 # <https://github.com/paleolimbot/rbbt/issues/47>). If not, install `rbbt`
 # from @wmoldham fork `remotes::install_github("wmoldham/rbbt", force = TRUE)`.
 
-quartor::bbt_write_quarto_bib(
-  bib_file = here::here("references.bib"),
+bbt_write_quarto_bib(
+  bib_file = here("references.bib"),
   dir = c("."),
   pattern = "\\.qmd$",
-  wd = here::here()
+  wd = here()
 )
